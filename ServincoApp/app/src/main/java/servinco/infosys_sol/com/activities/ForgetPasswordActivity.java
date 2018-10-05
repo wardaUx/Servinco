7b152830662effe802e00ec068f75e1ed6ff4d70
Umair <umairmajeed719@gmail.com>
2018-10-05 18:51:02 +0500

    Forget And Reset Password Screens added

---
+++ ServincoApp/app/src/main/java/servinco/infosys_sol/com/activities/ForgetPasswordActivity.java
@@ -0,0 +1,15 @@
+package servinco.infosys_sol.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import servinco.infosys_sol.com.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
---
+++ ServincoApp/app/src/main/java/servinco/infosys_sol/com/activities/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package servinco.infosys_sol.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import servinco.infosys_sol.com.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
