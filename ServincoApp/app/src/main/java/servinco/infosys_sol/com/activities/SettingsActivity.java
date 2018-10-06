c06e86a3cb3b4750cfb57f161e5add618e27dae7
AAwaisYaseen <aawaisyaseen@gmail.com>
2018-10-05 22:10:12 -0700

    Setting screen added

---
+++ ServincoApp/app/src/main/java/servinco/infosys_sol/com/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package servinco.infosys_sol.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import servinco.infosys_sol.com.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
